a6b8be2522079fac4daacd032b984a37dc6c8fb4
Noah <vik_026@mail.ru>
2022-02-23 16:10:41 +0300

    Firdavs bro

---
+++ firdavs/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+"); 
+
+int xa = 1, ya = 1,
+    xb = 9, yb = 9,
+    xc = 8, yc = 8;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
